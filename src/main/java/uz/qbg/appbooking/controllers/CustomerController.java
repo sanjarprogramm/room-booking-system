bb7897c0c10413c6b1992829e0d0ecc152a9958c
UsmanovSanjarbek <sanjar.qodiruli@gmail.com>
2023-05-30 17:04:19 +0500

    small improvements

--- src/main/java/uz/qbg/appbooking/controllers/CustomerController.java
+++ src/main/java/uz/qbg/appbooking/controllers/CustomerController.java
@@ -2,7 +2,6 @@ package uz.qbg.appbooking.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
